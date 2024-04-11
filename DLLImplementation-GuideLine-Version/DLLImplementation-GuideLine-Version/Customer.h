b7668ecb7ddfc50252d84e91923e0bd4949d98cd
Jae An <cube482@gmail.com>
2024-04-11 17:18:43 -0400

    Starting out with Customer class header

---
+++ DLLImplementation-GuideLine-Version/DLLImplementation-GuideLine-Version/Customer.h
@@ -0,0 +1,33 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+#include <string>
+class Customer
+{
+private:
+	int id;
+	std::string first;
+	std::string last;
+	double balance;
+	double bonus;
+	Customer* prev;
+	Customer* next;
+public:
+	int getId();
+	std::string getFirst();
+	std::string getLast();
+	double getBalance();
+	double getBonus();
+	Customer* getPrev();
+	Customer* getNext();
+	void setFirst(std::string);
+	void setLast(std::string);
+	void setBalance(double);
+	void setBonus(double);
+	void addBalance(double);
+	void subBalance(double);
+	void addBonus(double);
+	void subBonus(double);
+	void setPrev(Customer*);
+	void setNext(Customer*);
+};
+#endif
